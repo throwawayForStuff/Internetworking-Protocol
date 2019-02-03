8c8d2bb3e0a6f957b0965a64d0b415daffb53302
Phuong Pham <phuong@augusta.cs.pdx.edu>
2019-02-03 15:53:10 -0800

    Server

---
+++ Server.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+#include <sys/socket.h>
+
+using namespace std;
+
+int main()
+{
+  struct sockaddr_in myaddr;
+  struct sockaddr_in 
+  int fd; //Our socket
+  cout << "Hello World" << endl;
+
+  //To create socket, using socket system call
+  int s = (domain, type, protocol);
+  
+
+  return 0;
+}
+
