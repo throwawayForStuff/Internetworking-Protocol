c95beee84651e8d6f1cdf7bd5601e740ecf6f815
Phuong Pham <phuong@augusta.cs.pdx.edu>
2019-02-03 15:22:29 -0800

    Next

--- Project.cpp
+++ Project.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <cctype>
+#include <cstring>
+#include <sys/socket.h>
+
 using namespace std;
 
 int main()
 {
-    return 0;
+  cout << "Hello World" << endl;
+
+  //To create socket, using socket system call
+  int s = (domain, type, protocol);
+  
+
+  return 0;
 }
 
