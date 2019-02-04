95bc4246543f0adb9b581122d6222ffe5f7667ab
Phuong Pham <phuong@augusta.cs.pdx.edu>
2019-02-03 16:04:54 -0800

    It compiles!

--- Server.c
+++ Server.c
@@ -1,16 +1,23 @@
-#include <iostream>
-#include <cctype>
-#include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
-using namespace std;
+#define PORT 1080
+#define MAX  1024
 
 int main()
 {
   struct sockaddr_in serveraddr;
   struct sockaddr_in clientaddr;
   int socketfd; //Our socket
-  cout << "Hello World" << endl;
+  char buffer[MAX];
+  char *hello = "Hello World";
+  printf("\nHello World.\n");
 
   //To create socket, using socket system call
   //int s = (domain, type, protocol);
